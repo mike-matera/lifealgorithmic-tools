13125d933652cb07853473a1bbcd61efe1dc0b0c
Mike Matera <maximus@fatboycentral.com>
2021-09-03 11:01:05 -0700

    Update to make secrets command line executable

--- lifealgorithmic/secrets.py
+++ lifealgorithmic/secrets.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3 
+
 """
 Library for generating and managing copy-and-paste confirmation messages.
 """
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="lifealgorithmic",
-    version="0.0.2",
+    version="0.0.3",
     author="Mike Matera",
     author_email="matera@lifealgorithmic.com",
     description="My Course Tools",
