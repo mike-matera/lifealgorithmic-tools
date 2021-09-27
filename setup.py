0f1d11d51b6b2c157a08f1ea33bc2209daf1022d
Mike Matera <matera@lifealgorithmic.com>
2021-09-27 11:03:57 -0700

    Update for Linux test API

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="lifealgorithmic",
-    version="0.0.3",
+    version="0.1.0",
     author="Mike Matera",
     author_email="matera@lifealgorithmic.com",
     description="My Course Tools",
